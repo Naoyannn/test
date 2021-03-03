e6ff9470dc64eff674fe07529fea0b295f3c4e05
John Doe <johndoe@example.com>
2021-03-03 16:34:43 +0900

    0303

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,41 @@
 import 'ol/ol.css';
+import ImageWMS from 'ol/source/ImageWMS';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import {Image as ImageLayer, Tile as TileLayer} from 'ol/layer';
+
+var layers = [
+  new TileLayer({
+    source: new OSM(),
+  }),
+
+  /*
+  new ImageLayer({
+    extent: [-13884991, 2870341, -7455066, 6338219],
+    source: new ImageWMS({
+      url: 'https://ahocevar.com/geoserver/wms',
+      params: {'LAYERS': 'topp:states'},
+      ratio: 1,
+      serverType: 'geoserver',
+    }),
+  })
+  */
+
+];
+var map = new Map({
+  layers: layers,
+  target: 'map',
+  view: new View({
+    center: [-10997148, 4569099],
+    zoom: 4,
+  }),
+});
+
+
+
+/*
+import 'ol/ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
@@ -15,3 +52,4 @@ const map = new Map({
     zoom: 0
   })
 });
+*/
