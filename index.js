eddac05c7942f79a53b5bace81eb6aa702514414
John Doe <johndoe@example.com>
2021-03-03 09:26:20 +0900

    sss

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ var database = "gisdb" // database name
 var conString = "postgres://"+username+":"+password+"@"+host+"/"+database; // Your Database Connection
 
 
-//
-
 
 // Set up your database query to display GeoJSON
 var data = "SELECT * FROM a26-10_27-g_sedimentdisasterhazardarea_surface";
